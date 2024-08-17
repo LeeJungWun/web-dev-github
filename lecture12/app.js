23a5c98dc3ad9a14816a900635513f3eb7a97dc4
LeeJungWun <wun916@naver.com>
2024-08-18 00:28:14 +0900

    데모 연습

--- lecture12/app.js
+++ lecture12/app.js
@@ -10,7 +10,14 @@ paragraphElement.addEventListener('click', changeParagraphText);
 let inputElement = document.querySelector('input');
 
 function retrieveUserInput() {
-    let enteredText = inputElement.value;
-    console.log(enteredText);
+    // let enteredText = inputElement.value;
+    //console.log(enteredText);
+    let maxTextLength = 60;
+    let textLength = inputElement.value.length;
+    let spanElement = document.querySelector('span');
+    spanElement.innerHTML = textLength + '/' + maxTextLength;
+    
 }
 inputElement.addEventListener('input', retrieveUserInput);
+
+let spanElement = document.querySelector('span');
