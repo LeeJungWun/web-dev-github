df3151f4a6a2dfaa6cf49771cd52dae37accd095
LeeJungWun <wun916@naver.com>
2024-08-17 23:43:11 +0900

    새 HTML요소 추가&삭제

--- lecture12/app.js
+++ lecture12/app.js
@@ -7,3 +7,13 @@ anchorElement.href = 'https://google.com';
 
 anchorElement = document.querySelector('p a'); // p a { color: red; }
 anchorElement.href = 'https://academind.com';
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://google.com';
+newAnchorElement.textContent = 'This leads to Google';
+
+let firstParagraph = document.querySelector('p');
+firstParagraph.append(newAnchorElement);
+
+let firstH1aragraph = document.querySelector('H1');
+firstH1aragraph.remove();
