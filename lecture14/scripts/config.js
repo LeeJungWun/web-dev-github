3a16554bdc3b2903a057148a6f63841427423356
LeeJungWun <wun916@naver.com>
2024-09-08 00:46:58 +0900

    사용자 입력확인

--- lecture14/scripts/config.js
+++ lecture14/scripts/config.js
@@ -6,4 +6,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    configErrorsElement.textContent = '';
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayerName = formData.get('username').trim();
+
+    if (!enteredPlayerName) {
+        event.target.firstElementChild.classList.add('error');
+        configErrorsElement.textContent = 'Please enter a valid name';
+        return;
+    }
 }
