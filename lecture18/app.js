281e41eed16f4b2d15c0e2a4439c9f092370ecc1
LeeJungWun <wun916@naver.com>
2024-09-20 00:02:33 +0900

    ejs 반복 콘텐츠 출력

--- lecture18/app.js
+++ lecture18/app.js
@@ -1,47 +1,54 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req, res){
-    res.render('index');
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
-app.get('/restaurants', function(req, res){
-    res.render('restaurants');
+app.get("/restaurants", function (req, res) {
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storeRestaurants = JSON.parse(fileData);
+  res.render("restaurants", {
+    numberOfRestaurants: storeRestaurants.length,
+    restaurants: storeRestaurants,
+  });
 });
 
-app.get('/recommend', function(req, res){
-    res.render('recommend');
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
 });
 
-app.post('/recommend', function(req, res){
-    const restaurant = req.body;
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
 
-    const filePath =  path.join(__dirname, 'data', 'restaurants.json');
+  const filePath = path.join(__dirname, "data", "restaurants.json");
 
-    const fileData = fs.readFileSync(filePath);
-    const storeRestaurants = JSON.parse(fileData);
-    
-    storeRestaurants.push(restaurant);
-    fs.writeFileSync(filePath, JSON.stringify(storeRestaurants));
+  const fileData = fs.readFileSync(filePath);
+  const storeRestaurants = JSON.parse(fileData);
 
-    res.redirect('confirm');
+  storeRestaurants.push(restaurant);
+  fs.writeFileSync(filePath, JSON.stringify(storeRestaurants));
+
+  res.redirect("confirm");
 });
 
-app.get('/confirm', function(req, res){
-    res.render('confirm');
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
 });
 
-app.get('/about', function(req, res){
-    res.render('about');
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000);
